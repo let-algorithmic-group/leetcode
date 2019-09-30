be62679434d14b469f85894f87fa135a9e547f2b
xuuiao <xuuiao@gmail.com>
2019-09-30 14:29:29 +0800

    作业提交

---
+++ w2_400/xuuiao/findNthDigit.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := 31
+	fmt.Println(findNthDigit(a))
+	fmt.Println("findNthDigit")
+}
+
+func findNthDigit(n int) int {
+	if n < 10 {
+		return n
+	}
+	x, y := getX(n)
+	//fmt.Println("x,y:", x, y)
+	leftNum := n - y
+	//fmt.Println("leftDigit:", leftNum)
+	line := (leftNum - 1) / ((x + 1) * 10)
+	//fmt.Println("line:", line)
+	lineIndex := int(math.Mod(float64(leftNum-1), float64((x+1)*10)))
+	//fmt.Println("lineIndex:", lineIndex)
+	lineNum := lineIndex / (x + 1)
+	//fmt.Println("当前行第几个数字：", lineNum)
+	lineNumIndex := int(math.Mod(float64(lineIndex), float64(x+1)))
+	//fmt.Println("数字中第几个数", lineNumIndex)
+	theNumber := int(math.Pow10(x)) + line*10 + lineNum
+	//fmt.Println("定位到的数字：", theNumber)
+	return int(math.Mod(float64(theNumber/(int(math.Pow10((x+1)-lineNumIndex-1)))), 10))
+}
+
+//
+func getX(n int) (int, int) {
+	x := 0
+	y := 0
+	var maxNum int
+	for {
+		maxNum = (x + 1) * 9 * int(math.Pow10(x))
+		if maxNum > n {
+			break
+		} else {
+			y = y + maxNum
+			x++
+		}
+	}
+	return x, y
+}
