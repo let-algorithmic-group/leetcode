84c051660589a0257dbe879e515d8971ca184d7f
tory <haishangtao@hotmail.com>
2019-10-25 11:55:19 +0800

    tory提交

--- w5_array_33/tory/search-in-rotated-sorted-array.py
+++ w5_array_33/tory/search-in-rotated-sorted-array.py
@@ -11,12 +11,12 @@ class Solution:
             if nums[m] == target:
                 return m
             if nums[l] <= nums[m]:
-                if target >= nums[l] and nums[m] > target:
+                if target >= nums[l] and target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
-                if nums[m] < target and target <= nums[r]:
+                if target > nums[m] and target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
