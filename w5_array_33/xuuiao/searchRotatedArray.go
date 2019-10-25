08d0e437f7d3ea32535c88d1dc19238281b34702
xuuiao <xuuiao@gmail.com>
2019-10-25 18:57:09 +0800

    Update searchRotatedArray.go

--- w5_array_33/xuuiao/searchRotatedArray.go
+++ w5_array_33/xuuiao/searchRotatedArray.go
@@ -14,6 +14,9 @@ func main() {
 func search(nums []int, target int) int {
 	start := 0
 	end := len(nums) - 1
+	if end < 0 {
+		return -1
+	}
 	mid := (start + end) / 2
 	if nums[start] == target {
 		return start
