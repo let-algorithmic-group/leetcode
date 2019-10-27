d9d3ac92ca435ca1d8c601a272cf5adfe0ef1afa
sachinsong <sachin_song@163.com>
2019-10-27 17:06:53 +0800

    sachin

---
+++ w5_array_33/sachin/33.php
@@ -0,0 +1,39 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target)
+    {
+        $length = count($nums);
+        $left = 0;
+        $right = $length - 1;
+
+        while ($left <= $right) {
+            $mid = floor(($left + $right) / 2);
+            if ($nums[$mid] == $target) {
+                return $mid;
+            } elseif ($nums[$mid] < $nums[$right]) {
+                if ($nums[$mid] < $target && $target <= $nums[$right]) {
+                    $left = $mid + 1;
+                } else {
+                    $right = $mid - 1;
+                }
+            } else {
+                if ($nums[$left] <= $target && $target < $nums[$mid]) {
+                    $right = $mid - 1;
+
+                } else {
+                    $left = $mid + 1;
+
+                }
+            }
+        }
+        return -1;
+    }
+}
