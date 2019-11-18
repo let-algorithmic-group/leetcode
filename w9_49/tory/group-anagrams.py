b40b21cbf9e55edc0440a2177c8e707d10e9a01d
tory <haishangtao@hotmail.com>
2019-11-18 16:53:33 +0800

    tory提交

--- w9_49/tory/group-anagrams.py
+++ w9_49/tory/group-anagrams.py
@@ -1,33 +1,8 @@
-class HashObject:
-    def __init__(self, number):
-        self.number = number
 
-
-class Solution:
-    def singleNumber(self, nums) -> int:
-        max_number = max(nums)
-        a_list = [0 for i in range(max_number + 1)]
-        b_list = [0 for i in range(max_number + 1)]
-        for i in nums:
-            if i in b_list:
-                temp = b_list[i]
-                b_list[i] = temp + 1
-            else:
-                b_list[i] = 1
-            a_list[i] = i
-        print(a_list)
-        print(b_list)
-        for i in range(len(b_list)):
-            if b_list[i] == 1:
-                return a_list[i]
-
-    def getHash(self, hashObject: HashObject, listSize):
-        h = hash(hashObject)
-        hs = h ^ (h > 16)
-        return (listSize - 1) & hs
-
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.singleNumber([1]))
-    print(s.getHash(HashObject(-3),5))
+import collections
+def groupAnagrams(strs):
+    dic = collections.defaultdict(list)
+    for s in strs:
+        _s = ''.join(sorted(s))
+        dic[_s].append(s)
+    return dic.values()
