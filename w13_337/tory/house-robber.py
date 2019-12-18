1972bfff24bb58f99f6ace704c1b907ecc3264cd
tory <haishangtao@hotmail.com>
2019-12-18 11:09:31 +0800

    tory提交

--- w13_337/tory/house-robber.py
+++ w13_337/tory/house-robber.py
@@ -4,5 +4,14 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def rob(self, root: TreeNode) -> int:
+        return max(self.getRob(root))
+
+    def getRob(self, root: TreeNode) -> List[int]:
+        if not root:
+            return [0, 0]
+        l = self.getRob(root.left)
+        r = self.getRob(root.right)
+        return [max(l) + max(r), root.val + l[0] + r[0]]
